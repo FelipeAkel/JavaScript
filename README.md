501e58267db2ca139cbcc4948220cd698bddb954
FelipeAkel <felipe.akel01@gmail.com>
2023-11-24 23:40:14 -0300

    Atualização do README

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
-# JavaScript
+<p align="center"><img src="java-script.jpg" width="400"></p>
 
-Exercícios e exemplos do estudo em JavaScript
+## Lógica de Programação e Algoritmos com JavaScript
 
-# Livro
-Lógica de Programação e Algoritmos com JavaScript
+Repositório contendo os algoritmos de JavaScript abordados neste livro. 
+Fonte de estudo e aprofundamento da linguagem JavaScript.
 
-Teste Commit 
+## Git e GitHub
 
-Versão 1.0.0
+Revisão dos comando Git
+- Merge das branchs com o repositório principal main;
+- Criação de tags;
+- Análise dos commits;
+
+
+<p align="center"><b>Versão 1.0.0</b></p>
--- cap02/js/ex2_3.js
+++ cap02/js/ex2_3.js
@@ -15,12 +15,8 @@ form.addEventListener("submit", (e) => {
     retornoVeiculo.innerText = `Promoção: ${veiculo}`;
     retornoPromocao.innerText = `Entrada de R$ ${entrada.toFixed(2)} \n +12x de R$ ${parcelas.toFixed(2)}`;
 
-    // console.log(entrada, typeof entrada);
-    // console.log(parcelas, typeof parcelas);
-
     e.preventDefault();
 });
 
-
-// <!-- Elabora um programa para revenda de veículos. O programa deve ler modelo e preço do veículo. 
-// Apresentar como resposta o valor da entrada (50%) e o saldo em 12x. -->
+// console.log(entrada, typeof entrada);
+// console.log(parcelas, typeof parcelas);
